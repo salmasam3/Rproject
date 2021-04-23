d6b3dd26a327a2cd6b84d11723f644359c09327f
James Umbanhowar <jumbanho@unc.edu>
2021-04-23 15:03:27 -0400

    Got code working with no errors

--- mammal_data_process.R
+++ mammal_data_process.R
@@ -1,18 +1,20 @@
 mean_mass <- function (masses)
 {
-#Return the mean of a list of masses
-mean_mass <- mean(masses, na.rm = TRUE)
-return(masses)
+    #Return the mean of a list of masses
+    mean_mass <- mean(masses, na.rm = TRUE)
+    return(mean_mass)
 }
 
 all_of_it <- function ()
 {
     all_data <- read.table("MOMv3.3.txt", sep ="\t", 
-                           col.names=c("continent", "status", "order", "family","genus", "species", "log10mass", "mass", "ref"))
+                           col.names=c("continent", "status", "order", "family","genus", "species", "log10mass", "mass", "ref"), na.strings = "-999", stringsAsFactors = FALSE)
     
     continents <- all_data$continent
     status <- all_data$status
     masses <-  all_data$mass
+    
+    continents <- ifelse(continents == "Af", "AF", continents)
     
     results <- NULL
     for (continent in unique(continents))
@@ -25,9 +27,21 @@ all_of_it <- function ()
         results <- rbind(results, c(continent, avg_extant_mass, avg_extinct_mass, diff))
     }
     
-    for (line in all_data)
+    for (line in 1:dim(results)[2])
     {
-        print(line)
+        print(results[line,])
     }
-    
+    results
 }
+
+
+good <- all_of_it()
+good
+data.frame(good) -> good
+names(good) <- c("continent", "avg_extant_mass", "avg_extinct_mass", "diff")
+good$continent <- as.character(good$continent)
+good$diff <- as.numeric(as.character(good$diff))
+good$avg_extinct_mass <- as.numeric(as.character(good$avg_extinct_mass))
+good$avg_extant_mass <- as.numeric(as.character(good$avg_extant_mass))
+
+write.csv(good, "good.csv", row.names = FALSE)
